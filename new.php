c689c0fcc77c8587fb3ebc26a4b9abab7b8052ec
MallikarjunagoudaCM <64705254+MallikarjunagoudaCM@users.noreply.github.com>
2021-03-24 12:16:04 +0530

    HTML File added

--- new.php
+++ new.php
@@ -1,12 +1,29 @@
-<!DOCTYPE html>
-<html>
-<body>
+<HTML>
 
-<h1>My first PHP page</h1>
+<HEAD><TITLE>Your Title Here</TITLE></HEAD>
+<BODY BGCOLOR="FFFFFF">
+<CENTER><IMG SRC="clouds.jpg" ALIGN="BOTTOM"> </CENTER>
+<HR>
+<a href="http://somegreatsite.com">Link Name</a>
 
-<?php
-echo "Hello World!";
-?> 
+is a link to another nifty site
 
-</body>
-</html>
+<H1>This is a Header</H1>
+
+<H2>This is a Medium Header</H2>
+
+Send me mail at <a href="mailto:support@yourcompany.com">
+
+support@yourcompany.com</a>.
+
+<P> This is a new paragraph!
+
+<P> <B>This is a new paragraph!</B>
+
+<BR> <B><I>This is a new sentence without a paragraph break, in bold italics.</I></B>
+
+<HR>
+
+</BODY>
+
+</HTML>
